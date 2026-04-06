8684fd280b3eb58c141c57d19684a108b2b04160
VICTORHUGOLOPEZ <victor.hugo.lopez@hotmail.com>
2026-04-06 16:52:54 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>🏡 Tenant Portal</h1>
+      <p>Welcome to your rental dashboard</p>
+	  <p>4830 Fiadore Ln, Castle Rock, CO 80104</p>
+    </div>
+  );
+}
+
+export default App;
